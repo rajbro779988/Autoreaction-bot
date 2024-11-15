36c4c31bf79471b19e452ebe02792541ca70d118
Unstoppable Sanchit <137860157+Sanchit0102@users.noreply.github.com>
2024-11-15 19:33:52 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -18,7 +18,7 @@ TelegramBot = Client(
     plugins={'root': 'bot/plugins'}
 ) 
 
-app = web.AppRunner(await web_server())
+app = web.AppRunner(web_server())
 await app.setup()
 bind_address = "0.0.0.0"
 await web.TCPSite(app, bind_address, 8080).start()      
