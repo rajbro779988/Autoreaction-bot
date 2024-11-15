a8d1c049fc6b1cec6da42827e9f9e211c1e9d3aa
Unstoppable Sanchit <137860157+Sanchit0102@users.noreply.github.com>
2024-11-15 19:05:55 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -2,6 +2,8 @@ from pyrogram import Client
 from logging import getLogger
 from logging.config import dictConfig
 from .config import Telegram, LOGGER_CONFIG_JSON
+from aiohttp import web
+from route import web_server
 
 dictConfig(LOGGER_CONFIG_JSON)
 
@@ -14,4 +16,13 @@ TelegramBot = Client(
     api_hash=Telegram.API_HASH,
     bot_token=Telegram.BOT_TOKEN,
     plugins={'root': 'bot/plugins'}
-)
+) 
+
+app = web.AppRunner(await web_server())
+await app.setup()
+bind_address = "0.0.0.0"
+await web.TCPSite(app, bind_address, 8080).start()      
+print(f'Bot Started...🍃')
+
+app = TelegramBot()
+app.run()
