2e3cbcb64a4a49d265c9f9feefe76df0631a0990
Unstoppable Sanchit <137860157+Sanchit0102@users.noreply.github.com>
2024-11-15 18:08:57 +0530

    Fix script

--- bot/static.py
+++ bot/static.py
@@ -1,18 +1,14 @@
-WelcomeText = \
-"""
-ʜᴇʟʟᴏ %(first_name)s 👋,
+WelcomeText = """
+<b>ʜᴇʟʟᴏ {} 👋,
 
-**ɪ ᴀᴍ sɪᴍᴘʟᴇ ʙᴜᴛ ᴘᴏᴡᴇʀꜰᴜʟʟ ᴀᴜᴛᴏ ʀᴇᴀᴄᴛɪᴏɴ ʙᴏᴛ.
+ɪ ᴀᴍ sɪᴍᴘʟᴇ ʙᴜᴛ ᴘᴏᴡᴇʀꜰᴜʟʟ ᴀᴜᴛᴏ ʀᴇᴀᴄᴛɪᴏɴ ʙᴏᴛ.
 
 ᴊᴜsᴛ ᴀᴅᴅ ᴍᴇ ᴀs ᴀ ᴀᴅᴍɪɴ ɪɴ ʏᴏᴜʀ ᴄʜᴀɴɴᴇʟ ᴏʀ ɢʀᴏᴜᴘ ᴛʜᴇɴ sᴇᴇ ᴍʏ ᴘᴏᴡᴇʀ
 
-<blockquote>🧑‍💻 ᴍᴀɪɴᴛᴀɪɴᴇᴅ ʙʏ : <a href='https://t.me/Sb_botz_update'>sʙ ʙᴏᴛᴢ ᴜᴘᴅᴀᴛᴇ 🤞</a></blockquote>**"""
+<blockquote>🧑‍💻 ᴍᴀɪɴᴛᴀɪɴᴇᴅ ʙʏ : <a href='https://t.me/Sb_botz_update'>sʙ ʙᴏᴛᴢ ᴜᴘᴅᴀᴛᴇ 🤞</a></blockquote></b>"""
 
-Help = \
-"""
-
+Help = """
 <b><i>💞  ᴅᴏɴᴛ ɴᴇᴇᴅ ᴍᴀɴʏ ᴄᴏᴍᴍᴀɴᴅs ᴛᴏ ᴜsᴇ ᴛʜɪs ʙᴏᴛ 🤩.  ᴊᴜsᴛ ᴀᴅᴅ ᴍᴇ ᴄʜᴀɴɴᴇʟ ᴀɴᴅ ɢʀᴏᴜᴘ ɪ ᴡɪʟʟ ɢɪᴠᴇ ʏᴏᴜ ʟᴏᴠᴇʟʏ & ғᴜɴɴʏ ʀᴇᴀᴄᴛɪᴏɴ 🔥 , ʏᴏᴜ ᴄᴀɴ ᴜsᴇ ᴍᴇ ɪɴ ʏᴏᴜʀ ᴄʜᴀɴɴᴇʟ ᴀɴᴅ ɢʀᴏᴜᴘ..ᴊᴜsᴛ ᴀᴅᴅ ᴍᴇ ᴀɴᴅ ᴍᴀᴋᴇ ᴍᴇ ᴀᴅᴍɪɴ ᴀɴᴅ sᴇᴇ ᴍʏ ᴍᴀɢɪᴄ 😍</i></b>
 
-<blockquote>🧑‍💻 ᴍᴀɪɴᴛᴀɪɴᴇᴅ ʙʏ : <a href='https://t.me/Sb_botz_update'>sʙ ʙᴏᴛᴢ ᴜᴘᴅᴀᴛᴇ 🤞</a></blockquote>**
-
+<b><blockquote>🧑‍💻 ᴍᴀɪɴᴛᴀɪɴᴇᴅ ʙʏ : <a href='https://t.me/Sb_botz_update'>sʙ ʙᴏᴛᴢ ᴜᴘᴅᴀᴛᴇ 🤞</a></blockquote></b>
 """
